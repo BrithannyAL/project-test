9e435b5a9643b082aa2d6dbc814ea5979f63d36b
Brithanny Arguello <49217000+BrithannyAL@users.noreply.github.com>
2024-10-23 13:25:34 -0600

    change values

--- test/cypress/e2e/test.cy.ts
+++ test/cypress/e2e/test.cy.ts
@@ -8,15 +8,13 @@ describe('Login', function () {
 
     it('Login existoso', function () {
 
-        cy.get('input[name="username"]').type('admin').should('have.value', 'admin');
-        cy.get('input[name="password"]').type('admin').should('have.value', 'admin');
+        cy.get('input[name="username"]').type('guest@gmail.com').should('have.value', 'guest@gmail.com');
+        cy.get('input[name="password"]').type('guest').should('have.value', 'guest');
         cy.get('#userrole').select('CUSTOMER').should('have.value', 'customer');
 
         cy.xpath('/html/body/div[2]/div/form/div[6]/div/button').click();
 
         cy.url().should('include', '/shopping-cart/LoginSrv');
     })
-
-
 
 });
