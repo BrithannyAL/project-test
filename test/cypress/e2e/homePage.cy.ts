ecc732512591475195a2bc496cac2292924a9b85
Brithanny Arguello <49217000+BrithannyAL@users.noreply.github.com>
2024-10-23 14:53:06 -0600

    search product test

--- test/cypress/e2e/homePage.cy.ts
+++ test/cypress/e2e/homePage.cy.ts
@@ -1,12 +1,28 @@
-import { describe } from "node:test";
-
 describe("Home Page search", () => {
     beforeEach(() => {
-        cy.visit('http://localhost:8080/shopping-cart/index.jsp');
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            return false
+        });
+        cy.visit('http://localhost:8080/shopping-cart/login.jsp');
     });
 
     it('PR0032 - Búsqueda de productos', () => {
-        cy.get('input[name="search"]').type('apple').should('have.value', 'apple');
-        cy.get('button[name="searchButton"]').click();
+
+        //Se inicia sesión
+        cy.get('input[name="username"]').type('guest@gmail.com').should('have.value', 'guest@gmail.com');
+        cy.get('input[name="password"]').type('guest').should('have.value', 'guest');
+        cy.get('#userrole').select('CUSTOMER').should('have.value', 'customer');
+
+        cy.xpath('/html/body/div[2]/div/form/div[6]/div/button').click();
+
+        cy.url().should('include', '/shopping-cart/LoginSrv');
+
+        //Se busca un producto
+
+        cy.get('input[name="search"]').type('iphone').should('have.value', 'iphone');
+        cy.xpath('/html/body/div[1]/form/div/div/input').click();
+
+        cy.url().should('include', '/shopping-cart/index.jsp?search=iphone');
+        cy.get('div.row.text-center').children().should('have.length.greaterThan', 0);
     })
 })
